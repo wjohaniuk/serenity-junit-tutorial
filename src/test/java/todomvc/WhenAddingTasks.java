758088df208a3c6a696c47960e7d54daed70ddd2
wjohaniuk <32334253+wjohaniuk@users.noreply.github.com>
2022-11-09 10:31:05 +0100

    Initialize todomvc-start

---
+++ src/test/java/todomvc/WhenAddingTasks.java
@@ -0,0 +1,21 @@
+package todomvc;
+
+import org.junit.Test;
+
+public class WhenAddingTasks {
+
+    // TODO: Exercise 1
+    @Test
+    public void addingASingleTask() {
+        // Add "Feed The Cat" to the list
+        // Check that "Feed The Cat" appears in the list
+    }
+
+    // TODO: Exercise 2
+    @Test
+    public void addingMultipleTasks() {
+        // Add "Feed The Cat" and "Walk the dog" to the list
+        // Check that they all appear in the list
+    }
+
+}
