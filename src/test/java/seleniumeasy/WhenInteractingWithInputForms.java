035b1c02bc25127d7ea4c275fb470eddec1650d0
wjohaniuk <32334253+wjohaniuk@users.noreply.github.com>
2022-11-09 10:31:01 +0100

    Initialize html-elements

---
+++ src/test/java/seleniumeasy/WhenInteractingWithInputForms.java
@@ -0,0 +1,75 @@
+package seleniumeasy;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+/**
+ * This is a series of exercises designed to explore how to use
+ * Serenity BDD to test various kinds of HTML elements
+ */
+public class WhenInteractingWithInputForms {
+
+    /**
+     * Basic form fields:
+     * Enter a message and check that the message is correctly displayed
+     * https://www.seleniumeasy.com/test/basic-first-form-demo.html
+     */
+    @Test
+    public void basicForms() {
+    }
+
+    /**
+     * Basic form fields (part 2)
+     * Enter two values and calculate the result
+     * https://www.seleniumeasy.com/test/basic-first-form-demo.html
+     */
+
+    @Test
+    public void basicFormsWithMultipleFields() {
+    }
+
+    /**
+     * Checkboxes
+     * Check that a message appears when you click the checkbox
+     * https://www.seleniumeasy.com/test/basic-checkbox-demo.html
+     */
+    @Test
+    public void singleCheckbox() {
+    }
+
+    @Test
+    public void multipleCheckboxes() {
+    }
+
+    /**
+     * Radio buttons
+     * Check that a message appears when you click the radio button
+     * https://www.seleniumeasy.com/test/basic-radiobutton-demo.html
+     */
+    @Test
+    public void radioButtons() {
+    }
+
+    @Test
+    public void multipleRadioButtons() {
+    }
+
+    /**
+     * Dropdown lists
+     * https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html
+     */
+    @Test
+    public void selectList() {
+    }
+
+    /**
+     * Multi-Select Dropdown lists
+     * https://www.seleniumeasy.com/test/basic-select-dropdown-demo.html
+     */
+    @Test
+    public void multiSelectList() {
+    }
+}
