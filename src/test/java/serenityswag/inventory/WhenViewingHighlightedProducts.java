6ada051a66d323bcb3550ef9641d0c32bd604877
wjohaniuk <32334253+wjohaniuk@users.noreply.github.com>
2022-11-09 10:30:52 +0100

    Initialize locators-part-4-solution

---
+++ src/test/java/serenityswag/inventory/WhenViewingHighlightedProducts.java
@@ -0,0 +1,49 @@
+package serenityswag.inventory;
+
+import net.serenitybdd.junit.runners.SerenityRunner;
+import net.thucydides.core.annotations.Managed;
+import net.thucydides.core.annotations.Steps;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.WebDriver;
+import serenityswag.authentication.LoginActions;
+import serenityswag.authentication.User;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SerenityRunner.class)
+public class WhenViewingHighlightedProducts {
+
+    @Managed(driver = "chrome")
+    WebDriver driver;
+
+    @Steps
+    LoginActions login;
+
+    ProductListPageObject productList;
+
+    ProductDetailsPageObject productDetails;
+
+    @Test
+    public void shouldDisplayHighlightedProductsOnTheWelcomePage() {
+        login.as(User.STANDARD_USER);
+
+        List<String> productsOnDisplay = productList.titles();
+
+        assertThat(productsOnDisplay).hasSize(6)
+                                     .contains("Sauce Labs Backpack");
+    }
+
+    @Test
+    public void shouldDisplayCorrectProductDetailsPage() {
+        login.as(User.STANDARD_USER);
+
+        String firstItemName = productList.titles().get(0);
+
+        productList.openProductDetailsFor(firstItemName);
+
+        assertThat(productDetails.productName()).isEqualTo(firstItemName);
+    }
+}
